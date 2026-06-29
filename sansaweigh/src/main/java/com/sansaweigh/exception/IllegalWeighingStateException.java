44f63692bb000e1a56eced35deccdd3dca5b618f
qupardo-web <ezequielaraya33@gmail.com>
2026-06-28 23:43:16 -0400

    Commit directo para renombrar el proyecto (carpeta raiz se seguia llamando demo.)

---
+++ sansaweigh/src/main/java/com/sansaweigh/exception/IllegalWeighingStateException.java
@@ -0,0 +1,7 @@
+package com.sansaweigh.exception;
+
+public class IllegalWeighingStateException extends RuntimeException {
+    public IllegalWeighingStateException(String message) {
+        super(message);
+    }
+}
---
+++ sansaweigh/src/main/java/com/sansaweigh/model/CategoriaPeso.java
@@ -0,0 +1,20 @@
+package com.sansaweigh.model;
+
+public enum CategoriaPeso {
+    LIVIANO,
+    MEDIANO,
+    PESADO;
+
+    private static final double LIMITE_LIVIANO = 10.0;
+    private static final double LIMITE_MEDIANO = 50.0;
+
+    public static CategoriaPeso clasificar(double pesoEnSansas) {
+        if (pesoEnSansas <= LIMITE_LIVIANO) {
+            return LIVIANO;
+        } else if (pesoEnSansas <= LIMITE_MEDIANO) {
+            return MEDIANO;
+        } else {
+            return PESADO;
+        }
+    }
+}
